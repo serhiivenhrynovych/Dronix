aa0f41fab690d6e0e0255347570454ef9337c0a0
serhii <serhiivenhrynovych@gmail.com>
2019-01-24 09:45:53 +0200

    get images from ts, styled them

--- src/app/galery/galery.component.ts
+++ src/app/galery/galery.component.ts
@@ -13,10 +13,20 @@ export class GaleryComponent implements OnInit {
 
   ngOnInit() {
     this.imagesArr = [];
-    this.imagesArr.push({source: 'assets/photo/img1min.jpg', title: 'Title 1'});
-    this.imagesArr.push({source: 'assets/photo/img3min.jpg', title: 'Title 1'});
-    this.imagesArr.push({source: 'assets/photo/img5min.jpg', title: 'Title 1'});
-    this.imagesArr.push({source: 'assets/photo/img6min.jpg', title: 'Title 1'});
-      }
+    this.imagesArr.push({minImg: 'assets/photo/img1min.jpg', fullImg: 'assets/photo/img1.JPG', title: 'Title 1'});
+    this.imagesArr.push({minImg: 'assets/photo/img3min.jpg', fullImg: 'assets/photo/img3.jpeg', title: 'Title 2'});
+    this.imagesArr.push({minImg: 'assets/photo/img5min.jpg', fullImg: 'assets/photo/img5.JPG', title: 'Title 3'});
+    this.imagesArr.push({minImg: 'assets/photo/img6min.jpg', fullImg: 'assets/photo/img6.JPG', title: 'Title 4'});
+
+    this.imagesArr.push({minImg: 'assets/photo/img1min.jpg', fullImg: 'assets/photo/img1.JPG', title: 'Title 1'});
+    this.imagesArr.push({minImg: 'assets/photo/img3min.jpg', fullImg: 'assets/photo/img3.jpeg', title: 'Title 2'});
+    this.imagesArr.push({minImg: 'assets/photo/img5min.jpg', fullImg: 'assets/photo/img5.JPG', title: 'Title 3'});
+    this.imagesArr.push({minImg: 'assets/photo/img6min.jpg', fullImg: 'assets/photo/img6.JPG', title: 'Title 4'});
+    this.imagesArr.push({minImg: 'assets/photo/img1min.jpg', fullImg: 'assets/photo/img1.JPG', title: 'Title 1'});
+    this.imagesArr.push({minImg: 'assets/photo/img3min.jpg', fullImg: 'assets/photo/img3.jpeg', title: 'Title 2'});
+    this.imagesArr.push({minImg: 'assets/photo/img5min.jpg', fullImg: 'assets/photo/img5.JPG', title: 'Title 3'});
+    this.imagesArr.push({minImg: 'assets/photo/img6min.jpg', fullImg: 'assets/photo/img6.JPG', title: 'Title 4'});
+
+  }
 
 }
