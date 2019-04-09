be833658adac9ecf36b529cef6a5bcf9ed47a602
serhiivenhrynovych@gmail.com <serhiivenhrynovych@gmail.com>
2019-04-10 01:40:15 +0300

    added info for details

--- src/app/drones-catalog/drones-catalog.component.ts
+++ src/app/drones-catalog/drones-catalog.component.ts
@@ -11,6 +11,12 @@ export class DronesCatalogComponent implements OnInit {
 
   dronesArr = [];
   detailsArr = [];
+  detailsArr1 = [];
+  detailsArr2 = [];
+  detailsArr3 = [];
+  detailsArr4 = [];
+  detailsArr5 = [];
+  detailsArr6 = [];
 
   constructor(public dialog: MatDialog) { }
 
@@ -22,19 +28,57 @@ export class DronesCatalogComponent implements OnInit {
       {name: 'Detail4', price: 800, minImg: 'assets/photo/detail4.jpg', fullImg: 'assets/photo/img5.JPG'},
    ];
 
+    this.detailsArr1 = [
+      {name: 'Лопасті оригінал', price: 700, minImg: 'assets/photo/detail21.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Лопасті копія', price: 300, minImg: 'assets/photo/detail22.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Акумулятор оригінал', price: 2700, minImg: 'assets/photo/detail23.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Акумулятор копія', price: 2000, minImg: 'assets/photo/detail24.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
+    this.detailsArr2 = [
+      {name: 'Лопасті оригінал', price: 800, minImg: 'assets/photo/detail31.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Лопасті копія', price: 300, minImg: 'assets/photo/detail32.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Акумулятор оригінал', price: 3300, minImg: 'assets/photo/detail33.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
+    this.detailsArr3 = [
+      {name: 'Акумулятор оригінал', price: 2000, minImg: 'assets/photo/detail41.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
+    this.detailsArr4 = [
+      {name: 'Лопасті оригінал', price: 0, minImg: 'assets/photo/detail52.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Лопасті копія', price: 0, minImg: 'assets/photo/detail52.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Шлейф Phantom 3 Standart', price: 550, minImg: 'assets/photo/detail51.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Шлейф Phantom 3 Adv/PRO', price: 600, minImg: 'assets/photo/detail51.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
+    this.detailsArr5 = [
+      {name: 'Лопасті оригінал', price: 700, minImg: 'assets/photo/detail62.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Лопасті копія', price: 300, minImg: 'assets/photo/detail62.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Шлейф Phantom 4', price: 700, minImg: 'assets/photo/detail61.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Шлейф Phantom 4 PRO', price: 700, minImg: 'assets/photo/detail61.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
+    this.detailsArr6 = [
+      {name: 'Лопасті пар/компл. оригинальний', price: 600, minImg: 'assets/photo/detail11.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Мотори оригінальні', price: 3500, minImg: 'assets/photo/detail12.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Акумулятор 5100 мАч 17,4', price: 2400, minImg: 'assets/photo/detail13.jpg', fullImg: 'assets/photo/img5.JPG'},
+      {name: 'Камера оригінальна', price: 4000, minImg: 'assets/photo/detail14.jpg', fullImg: 'assets/photo/img5.JPG'},
+    ];
+
     this.dronesArr = [
-      {name: 'DJI Mavic PRO', price: 34000, minImg: 'assets/photo/dronMavicPro.jpg', fullImg: 'assets/photo/img5.JPG', charge: '20', controlLength: '3',
-        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
+      {name: 'DJI Mavic PRO', price: 32500, minImg: 'assets/photo/dronMavicPro.jpg', fullImg: 'assets/photo/img5.JPG', charge: '27', controlLength: '7',
+        speed: '65', sensorLength: '4.30', stabilizer: '3', pixels: '12', details: this.detailsArr1},
       {name: 'DJI Mavic 2 PRO/ZOOM', price: 34000, minImg: 'assets/photo/dronMavic2Pro.jpg', fullImg: 'assets/photo/img5.JPG', charge: '20', controlLength: '3',
-        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
-      {name: 'DJI Mavic Air', price: 68000, minImg: 'assets/photo/dronMavicAir.jpg', fullImg: 'assets/photo/img5.JPG', charge: '20', controlLength: '2',
-        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
+        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr2},
+      {name: 'DJI Mavic Air', price: 24300, minImg: 'assets/photo/dronMavicAir.jpg', fullImg: 'assets/photo/img5.JPG', charge: '21', controlLength: '2',
+        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr3},
       {name: 'DJI Phantom 3', price: 9000, minImg: 'assets/photo/dronPhantom3.jpg', fullImg: 'assets/photo/img5.JPG', charge: '30', controlLength: '4',
-        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
-      {name: 'DJI Phantom 4', price: 80000, minImg: 'assets/photo/dronPhantom4.jpg', fullImg: 'assets/photo/img5.JPG', charge: '40', controlLength: '1',
-        speed: '60', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
+        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr4},
+      {name: 'DJI Phantom 4', price: 49500, minImg: 'assets/photo/dronPhantom4.jpg', fullImg: 'assets/photo/img5.JPG', charge: '30', controlLength: '7',
+        speed: '72', sensorLength: '30', stabilizer: '4.60', pixels: '16', details: this.detailsArr5},
       {name: 'XIAOMI MI DRONE 4K', price: 34000, minImg: 'assets/photo/dronXiaomiMi.jpg', fullImg: 'assets/photo/img5.JPG', charge: '20', controlLength: '3',
-        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr},
+        speed: '50', sensorLength: '30', stabilizer: '2', pixels: '12', details: this.detailsArr6},
     ];
   }
 
